3bfbd3dd4c38f0947e4d6a96dd96a7e889a10a37
sidlovskyy <sidlovskyy@gmail.com>
2013-06-26 14:11:02 +0300

    First problem solved.

---
+++ src/ProjectEuler/P1.fs
@@ -0,0 +1,4 @@
+﻿module ProjectEulerP1
+
+let sumOfMultiples a1 a2 n =
+    [1 .. (n - 1)] |> List.filter (fun x -> (x % a1 = 0) || (x % a2 = 0)) |> List.sum    
---
+++ src/ProjectEuler/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+module ProjectEuler
+
+open ProjectEulerP1
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%d" (sumOfMultiples 3 5 1000)
+    0 // return an integer exit code
